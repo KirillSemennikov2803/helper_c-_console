186bfc0ded30a19f6e30ae93f39d85de6be22ad6
Your Name <Semennikov1360@mail.ru>
2020-02-18 22:30:02 +0300

    add some refactoring

--- helper_test_console/helper_test_console/HelperHttpClient.cs
+++ helper_test_console/helper_test_console/HelperHttpClient.cs
@@ -9,24 +9,41 @@ public enum HttpAnswer
 	auth_error,
 	server_is_not_available,
 	internal_server_error,
-	success
+	success,
+	strange
 }
-public class HelperHttpClient
+namespace HelperHttpClient
 {
-
-	public HelperHttpClient()
+	public class HelperHttpClient
 	{
 
-	}
+		public HelperHttpClient()
+		{
 
-	public static HttpAnswer auth(string login,string password_hash)
-    {
-		User.User_db_web tom = new User.User_db_web { login = login, password_hash = password_hash };
-		var client = new RestClient("http://127.0.0.1:8000/auth");
-		string json = JsonSerializer.Serialize<User.User_db_web>(tom);
-		var request = new RestRequest()
-			.AddJsonBody(json);
-		var response = client.Post(request);
-		return HttpAnswer.success;
+		}
+
+		public static HttpAnswer auth(string login, string password_hash)
+		{
+			User.User_db_web tom = new User.User_db_web { login = login, password_hash = password_hash };
+			var client = new RestClient("http://127.0.0.1:8000/auth");
+			string json = JsonSerializer.Serialize<User.User_db_web>(tom);
+			var request = new RestRequest()
+				.AddJsonBody(json);
+			var response = client.Post(request);
+
+			switch (response.StatusCode)
+			{
+				case 0:
+					return HttpAnswer.server_is_not_available;
+				case System.Net.HttpStatusCode.Unauthorized:
+					return HttpAnswer.auth_error;
+				case System.Net.HttpStatusCode.OK:
+					return HttpAnswer.success;
+				case System.Net.HttpStatusCode.InternalServerError:
+					return HttpAnswer.internal_server_error;
+				default:
+					return HttpAnswer.strange;
+			}
+		}
 	}
 }
--- helper_test_console/helper_test_console/Program.cs
+++ helper_test_console/helper_test_console/Program.cs
@@ -11,16 +11,10 @@ namespace helper_test_console
         static void Main(string[] args)
         {
 
-
-            var client = new RestClient("http://127.0.0.1:8000/auth");
-
-            User.User_db_web tom = new User.User_db_web { login = "admin", password_hash = "c4ca4238a0b923820dcc509a6f75849b" };
-            string json = JsonSerializer.Serialize<User.User_db_web>(tom);
-            var request = new RestRequest();
-            request.AddJsonBody(json);
-            var response = client.Post(request);
-            Console.WriteLine(json);
-            var a = 1;
+            var anser = HelperHttpClient
+                 .HelperHttpClient
+                 .auth("admin", "c4ca4238a0b923820dcc509a6f75849b");
+            Console.WriteLine(anser); 
 
         }
     }
